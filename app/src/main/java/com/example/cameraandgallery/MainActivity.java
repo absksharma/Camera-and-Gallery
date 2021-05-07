42079d13a91612dcf53eabd42590891403224307
Abhishek <absk.sharma7@gmail.com>
2021-05-07 09:05:57 +0530

    added permissions to access camera and External storage.

--- app/src/main/java/com/example/cameraandgallery/MainActivity.java
+++ app/src/main/java/com/example/cameraandgallery/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.cameraandgallery;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+    private static final int PERMISSION_CODE = 1000;
+    private static final int OPEN_GALLERY = 1002;
     ImageView iv;
     Button btn, btn2;
 
@@ -18,18 +29,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        iv = findViewById(R.id.imageView);
         btn = findViewById(R.id.button);
         btn2 = findViewById(R.id.button2);
-        iv = findViewById(R.id.imageView);
 
+        btn.setOnClickListener(view -> {
+            //runtime permission for Version >=M
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                        PackageManager.PERMISSION_DENIED ||
+                        checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                                PackageManager.PERMISSION_DENIED) {
+                    //For permission request
+                    String[] permission = {Manifest.permission.CAMERA, Manifest.permission.MANAGE_EXTERNAL_STORAGE};
+                    //pop up for permission
+                    requestPermissions(permission, PERMISSION_CODE);
+                } else {
+                    //permissions already granted
+                    openCamera();
+                }
+            } else {
+                //System <= M version
+                openCamera();
+            }
+        });
+
+        btn2.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, OPEN_GALLERY);
+        });
+    }
+
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
     }
 
     @Override
-    public void onClick(View view) {
-        if (view == btn) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                } else {
+                    Toast.makeText(this, "PERMISSIONS DENIED", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
-        } else if (view == btn2) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == IMAGE_CAPTURE_CODE) {
+            Bitmap bm = (Bitmap) data.getExtras().get("data");
+            iv.setImageBitmap(bm);
+        } else if (requestCode == OPEN_GALLERY) {
+            Uri u = data.getData();
+            iv.setImageURI(u);
         }
     }
 }
